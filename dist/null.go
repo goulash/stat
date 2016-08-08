a85c2bce8ec3a01583d3fd5ceff3e1e5c57bb1b5
Ben Morgan <neembi@gmail.com>
2016-08-08 15:47:40 +0200

    Adding Null distribution, everything about it is 0

---
+++ dist/null.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2016, Ben Morgan. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
+
+package dist
+
+type Null struct{}
+
+func NewNull() *Null             { return &Null{} }
+func (n *Null) Int63() int64     { return 0 }
+func (n *Null) Float64() float64 { return 0.0 }
+func (n *Null) Mean() float64    { return 0.0 }
+func (n *Normal) Var() float64   { return 0.0 }
+func (n *Normal) Std() float64   { return 0.0 }
+func (n *Null) String() string   { return "null" }
