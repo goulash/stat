152dab06d0864171fc9f6cddaf8f46244d4fd1f5
Ben Morgan <neembi@gmail.com>
2016-08-08 16:00:53 +0200

    Bugfix: copy&paste error

--- dist/null.go
+++ dist/null.go
@@ -6,10 +6,10 @@ package dist
 
 type Null struct{}
 
-func NewNull() *Null             { return &Null{} }
-func (n *Null) Int63() int64     { return 0 }
-func (n *Null) Float64() float64 { return 0.0 }
-func (n *Null) Mean() float64    { return 0.0 }
-func (n *Normal) Var() float64   { return 0.0 }
-func (n *Normal) Std() float64   { return 0.0 }
-func (n *Null) String() string   { return "null" }
+func NewNull() *Null            { return &Null{} }
+func (n Null) Int63() int64     { return 0 }
+func (n Null) Float64() float64 { return 0.0 }
+func (n Null) Mean() float64    { return 0.0 }
+func (n Null) Var() float64     { return 0.0 }
+func (n Null) Std() float64     { return 0.0 }
+func (n Null) String() string   { return "null" }
